cb25fe7744b0bdecc7ee074989d49064c9b80616
jonathanIckovich <90986058+jonathanIckovich@users.noreply.github.com>
2022-12-25 04:58:47 -0700

    fixing problem with twitter post not accepting dynamic non hrd coded values.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Provider } from "react-redux";
 import store from "./store";
 import Randomizer from "./Randomizer";
+import { PersistGate } from 'redux-persist/integration/react'
 function App() {
   return (
  <Provider store={store}>
-    <Randomizer />
+      <Randomizer />
  </Provider>
   );
 }
